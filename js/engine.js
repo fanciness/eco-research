5968a311a97e4d7ba97b752bf57e3d760074afc8
Alexander Pirozhkov <funkysolution@gmail.com>
2016-06-13 17:19:27 +0500

    js style

--- js/engine.js
+++ js/engine.js
@@ -22,9 +22,9 @@
     if (!e) e = window.event;
     var el = (e.srcElement || e.target);
 
-    if (el.className==='nav-h')
+    if (el.className === 'nav-h') {
       location.href = el.children[0].getAttribute('href');
-    else if (el.tagName.toUpperCase()==='SUMMARY') {
+    } else if (el.tagName.toUpperCase() === 'SUMMARY') {
       var clicked = el.id;
       if (opened === clicked) {
         _elem(clicked).close();
@@ -40,6 +40,6 @@
     }
   };
 
-  _elem('y').find().innerHTML=new Date().getFullYear();
+  _elem('y').find().innerHTML = new Date().getFullYear();
 
 })();
